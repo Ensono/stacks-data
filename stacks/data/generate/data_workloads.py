047b31516542a979078f6149b1deaf8f0fc175c6
james.watsohn@ensono.com <james.watsohn@ensono.com>
2023-12-11 14:42:26 +0000

    commit

--- stacks/data/generate/data_workloads.py
+++ stacks/data/generate/data_workloads.py
@@ -97,7 +97,7 @@ def generate_pipeline(validated_config: WorkloadConfigBaseModel, dq_flag: bool) 
     path_separator = get_os_path_separator()
     workload_type = validated_config.workload_type.lower()
     template_source_path = Path(TEMPLATES_DIRECTORY, workload_type, validated_config.template_source_folder)
-    template_source_path = (template_source_path, path_separator)
+    template_source_path = str(template_source_path) + path_separator
 
     target_dir = generate_target_dir(workload_type, validated_config.name)
 
@@ -119,7 +119,7 @@ def generate_pipeline(validated_config: WorkloadConfigBaseModel, dq_flag: bool) 
     if dq_flag:
         template_source_folder = f"{validated_config.template_source_folder}_DQ"
         template_source_path = os.path.join(TEMPLATES_DIRECTORY, workload_type, template_source_folder)
-        template_source_path = (template_source_path, path_separator)
+        template_source_path = str(template_source_path) + path_separator
         render_template_components(validated_config, template_source_path, target_dir)
     click.echo(f"Successfully generated workload components: {target_dir}")
 
