b2eae6a8fdc4ceebcceccfdfea0fb428019ab4cf
adurkan-amido <andy.durkan@amido.com>
2024-11-21 04:24:55 +0000

    add try except to optional dependencies

--- stacks/data/behave/shared_steps.py
+++ stacks/data/behave/shared_steps.py
@@ -6,12 +6,9 @@ workloads.
 import json
 import uuid
 from datetime import datetime
-
 import polling2
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.datafactory import DataFactoryManagementClient
-from behave import given, step, then
-from behave.runner import Context
 
 from stacks.data.constants import (
     ADLS_ACCOUNT,
@@ -26,6 +23,16 @@ from stacks.data.azure.data_factory import (
     create_adf_pipeline_run,
 )
 from stacks.data.azure.adls import AdlsClient
+
+try:
+    from behave import given, step, then
+    from behave.runner import Context
+except ImportError:
+    raise ImportError(
+        "Required dependencies for Stacks Data Behave testing are not installed. "
+        "Please install them using: pip install stacks-data[behave]"
+    )
+
 
 credential = DefaultAzureCredential()
 adf_client = DataFactoryManagementClient(credential, AZURE_SUBSCRIPTION_ID)
