945e63a47b75ddcdb61e8132db0ecccd187295ab
Paulina Motyl <paulina.motyl@ensono.com>
2025-07-09 09:26:49 +0200

    unit test

--- tests/unit/platforms/fabric/test_lakehouse.py
+++ tests/unit/platforms/fabric/test_lakehouse.py
@@ -1,3 +1,5 @@
+import pytest
+
 from stacks.data.platforms.fabric.lakehouse import LakehouseClient
 
 
@@ -18,3 +20,12 @@ def test_get_account_url():
     lakehouse_client = LakehouseClient(workspace_id, lakehouse_id)
     expected_url = "https://onelake.dfs.fabric.microsoft.com"
     assert lakehouse_client.get_account_url() == expected_url
+
+
+@pytest.mark.parametrize("directory_path", ["/path/to/directory", "path/to/directory/"])
+def test_get_full_directory_path(directory_path):
+    lakehouse_id = "lakehouse-08d36aeb-12d3"
+    workspace_id = "workspace-123e4567-e89b"
+    client = LakehouseClient(workspace_id, lakehouse_id)
+    expected_path = f"{lakehouse_id}/path/to/directory"
+    assert client._get_full_directory_path(directory_path) == expected_path
